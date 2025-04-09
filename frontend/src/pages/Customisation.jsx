b072a2a5f2481ef3077a794da0b964873100a069
Archak Nath <112867859+archakNath@users.noreply.github.com>
2025-04-10 02:07:23 +0530

    test: code injection

--- frontend/src/pages/Customisation.jsx
+++ frontend/src/pages/Customisation.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
 
 function Customisation() {
-  return (
-    <div>
-      <h2>This is the Customisation Page</h2>
+  const options = [
+    <div><h2>This is the Customisation Page1</h2></div>,
+    <div><h2>This is the Customisation Page2</h2></div>,
+  ];
+
+  const [customisation, setCustomisation] = React.useState(1);
+
+  const handleChange = () => {
+    setCustomisation((prev) => (prev + 1) % options.length);
+  };
+
+  return(
+    <div className='flex flex-col items-center justify-center h-screen'>
+    <button onClick={handleChange} className='bg-blue-500 text-white px-3 py-2 rounded-xl cursor-pointer'>Change</button>
+    {options[customisation]}
     </div>
-  )
+  );
 }
 
 export default Customisation

22d5a143e89c6492eb9a902b54d7a37ad72ac5c8
SrijitPal-Dev <151395008+SrijitPal-Dev@users.noreply.github.com>
2025-04-10 16:40:18 +0530

    SideBar Added Initially

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
-
+import Sidebar from '../components/sidebar'
 const Landing = () => {
   return (
     <div>
+      <Sidebar></Sidebar>
       Landing page
     </div>
   )
